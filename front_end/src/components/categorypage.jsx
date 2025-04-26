4ce77fcf9d5f33d59316fffd79b5958ea5beee2e
djrobin1963 <djrobin1963905@gmail.com>
2025-04-26 12:58:55 -0400

    Catergory Page CSS added

--- front_end/src/components/categorypage.jsx
+++ front_end/src/components/categorypage.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { apiFetch } from "../api/client";
 import MovieCard from "./moviecard";
+import "./categorypage.css";
 
 export default function CategoryPage() {
   const { category } = useParams();
