2d00647c7fe3d0eb4725194e49a050acd54d27e7
djrobin1963 <djrobin1963905@gmail.com>
2025-05-17 16:34:35 -0400

    Render Deploy Commit

--- back_end/db/client.js
+++ back_end/db/client.js
@@ -1,4 +1,10 @@
 const { Client } = require("pg");
-const client = new Client(process.env.DATABASE_URL || 3000);
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false,
+});
 
 module.exports = client;
